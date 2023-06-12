2b36f31068b9f5bd92ece422432eab814fa3976b
mzkyrwt8 <mzkyrwt8@gamil.com>
2023-06-12 23:25:33 +0300

    button  added.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+const submitBtn = document.getElementById('submit-btn');
+const nameInput = document.getElementById('name-input');
+const message = document.getElementById('message');
+
+submitBtn.addEventListener('click', () => {
+    const name = nameInput.value;
+    message.innerHTML = `Hello, ${name}!`;
+});
